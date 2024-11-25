3a91354e9adcf10af626b606c4c984cb66d10e15
rishabh732004 <rishabhdu7@gmail.com>
2024-11-25 16:52:24 +0530

    created compuer choice

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello")
+let userScore=0;
+let compScore=0;
+
+const choices=document.querySelectorAll(".choices")
+
+function getcomputerchoice(){
+  const options= ["rock","paper","scissor"];
+  const RandChoice= Math.floor(Math.random()*3);
+  return options[RandChoice];
+  
+}
+
+function GetUserChoice()=>{
+
+
+}
+
+getcomputerchoice();
+
+
