13a08f766e5cf7351821a354e12a39baaeb0ee66
rishabh732004 <rishabhdu7@gmail.com>
2024-11-27 12:21:55 +0530

    wrote a code to show winner

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 let userScore=0;
 let compScore=0;
 
-const choices=document.querySelectorAll(".choices")
+const choices=document.querySelectorAll(".choices");
 
 function getcomputerchoice(){
   const options= ["rock","paper","scissor"];
   const RandChoice= Math.floor(Math.random()*3);
   return options[RandChoice];
   
-}
+};
 
-function GetUserChoice()=>{
+ function getUserChoice(){
+  choices.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+      const userChoice=choice.getAttribute("id");
+    
+    })
+  })
+  
+  function draw(){
+    console.log("draw")
+  }
+ };
+function PlayGame(options,userChoice){
+ if (options===userChoice){
+   draw();
+ }
+ else {
+  let userwin;
+   if(userChoice==="rock"){
+    userwin= options=== "paper"? false :true;
+   } 
+   else if(userChoice==="paper"){
+    userwin= options=== "scissor"?true: false;
+   }
+   else{
+     userwin= options==="rock"? true:false;
+   }
+ }
 
-
-}
+ function winner(){
+  getcomputerchoice();
+  getUserChoice();
+  PlayGame(options,userChoice);
+  console.log(userwin)
+ }
+};
 
 getcomputerchoice();
+getUserChoice();
 
-
+ 
