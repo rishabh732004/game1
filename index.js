d69133b04b62a2f517818f76e41782c0f33f5935
rishabh732004 <rishabhdu7@gmail.com>
2024-11-30 11:07:32 +0530

    created  user and computer choices

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
+const choices=document.querySelectorAll(".choice");
+
 const getcompchoice=()=>{
   const options=["rock","paper","scissor"];
   const randIdx= Math.floor(Math.random()*3);
-  console.log(randIdx);
   return options[randIdx];
 }
 
 
- getcompchoice()
+choices.forEach((choice)=>{
+choice.addEventListener("click",()=>{
+const userChoice= choice.getAttribute("id");
+playGame(userChoice);
+})
+})
+
+
+const playGame=(userChoice)=>{
+  console.log("user choice is",userChoice);
+const compChoice=getcompchoice();
+console.log("computer choice is",compChoice);
+}
+playGame()
+ 
