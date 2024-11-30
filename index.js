74f63590b1e8b516dc33baadd3451af56995ebe6
rishabh732004 <rishabhdu7@gmail.com>
2024-11-30 13:10:19 +0530

    made some changes in html css and shown winner in js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+let user_score=0;
+let comp_score=0;
 
 const choices=document.querySelectorAll(".choice");
+const msg=document.querySelector("#msg");
+const userScore=document.querySelector("#userScore");
+const compScore=document.querySelector("#compScore");
+
 
 const getcompchoice=()=>{
   const options=["rock","paper","scissor"];
@@ -17,9 +23,53 @@ playGame(userChoice);
 
 
 const playGame=(userChoice)=>{
-  console.log("user choice is",userChoice);
 const compChoice=getcompchoice();
-console.log("computer choice is",compChoice);
+
+if(userChoice===compChoice){
+    msg.innerText="GAME DRAW";
+    msg.style.backgroundColor="yellow";
+    
+    
 }
-playGame()
+else{
+  let userWin=true;
+  if (userChoice==="rock"){
+   userWin= compChoice==="paper"? false:true;
+  }
+  else if(userChoice==="paper"){
+   userWin= compChoice==="scissor"? false: true;
+  }
+  else{
+    userWin= compChoice==="rock"? false:true;
+  }
+  showWinner(userWin);
+}
+
+}
+
+
+const showWinner=(userWin)=>{
+
+  if (userWin===true){
+    user_score++;
+    userScore.innerText=user_score;
+    console.log("you win")
+
+    msg.innerText="YOU WIN";
+    msg.style.backgroundColor= "green";
+    
+
+  }
+  else{
+    console.log("you lose");
+    comp_score++;
+    compScore.innerText= comp_score;
+    msg.innerText="YOU LOSE";
+    msg.style.backgroundColor="red";
+    
+  }
+}
+
+
+
  
