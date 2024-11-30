cd49fb0eb47f8b77b8f9268a284035965e945085
rishabh732004 <rishabhdu7@gmail.com>
2024-11-30 10:04:52 +0530

    created computer choice again

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-let userScore=0;
-let compScore=0;
 
-const choices=document.querySelectorAll(".choices");
+const getcompchoice=()=>{
+  const options=["rock","paper","scissor"];
+  const randIdx= Math.floor(Math.random()*3);
+  console.log(randIdx);
+  return options[randIdx];
+}
 
-function getcomputerchoice(){
-  const options= ["rock","paper","scissor"];
-  const RandChoice= Math.floor(Math.random()*3);
-  return options[RandChoice];
-  
-};
 
- function getUserChoice(){
-  choices.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-      const userChoice=choice.getAttribute("id");
-    
-    })
-  })
-  
-  function draw(){
-    console.log("draw")
-  }
- };
-function PlayGame(options,userChoice){
- if (options===userChoice){
-   draw();
- }
- else {
-  let userwin;
-   if(userChoice==="rock"){
-    userwin= options=== "paper"? false :true;
-   } 
-   else if(userChoice==="paper"){
-    userwin= options=== "scissor"?true: false;
-   }
-   else{
-     userwin= options==="rock"? true:false;
-   }
- }
-
- function winner(){
-  getcomputerchoice();
-  getUserChoice();
-  PlayGame(options,userChoice);
-  console.log(userwin)
- }
-};
-
-getcomputerchoice();
-getUserChoice();
-
- 
+ getcompchoice()
